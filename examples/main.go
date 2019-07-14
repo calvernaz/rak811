96da4345cb24ae8543143d3feb4602f83686b518
Cesar Alvernaz <calvernaz@weirdloop.org>
2019-07-14 21:34:02 +0100

    cleanup example

--- examples/main.go
+++ examples/main.go
@@ -1,64 +1,21 @@
 package main
 
 import (
-	"context"
-
+	"github.com/pkg/errors"
 	"github.com/tarm/serial"
 
 	"github.com/calvernaz/rak811"
 )
 
-type Options struct {
-	str string
-	// for alternative data
-	Context context.Context
-}
-
-type Option func(o *Options)
-
-// NewConfig returns new config
-func NewConfig(opts ...Option) *Config {
-	return newConfig(opts...)
-}
-
-type Config struct {
-	opts Options
-}
-
-func newConfig(opts ...Option) *Config {
-	options := Options{
-		str: "default",
-		Context: nil,
-	}
-
-	for _, o := range opts {
-		o(&options)
-	}
-
-	c := &Config{
-		opts:     options,
-	}
-
-	return c
-}
-
-func WithSource(s context.Context) Option {
-	return func(o *Options) {
-		o.Context = s
-	}
-}
-
-
-func WithString(s string) Option {
-	return func(o *Options) {
-		o.str = s
-	}
-}
-
 func main() {
 	cfg := &serial.Config{
 		Name:        "/dev/ttyAMA0",
 	}
-	_, _ = rak811.New(cfg)
 
+	lora, err := rak811.New(cfg)
+	if err != nil {
+		errors.Wrap(err, "failed to create rak811 instance")
+	}
+
+	lora.Version()
 }
