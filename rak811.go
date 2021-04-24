4e85ae04bd78f6ac31505c5aaf742280bd8c221f
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2021-04-24 12:12:16 +0100

    test debug messages output - trim suffix

--- rak811.go
+++ rak811.go
@@ -257,7 +257,7 @@ func (l *Lora) tx(s string, fn func([]byte) (string, error)) (string, error) {
 
 func debug(l *Lora, format string, a ...interface{}) {
 	if l.config.debug {
-		fmt.Println(format, a)
+		fmt.Println(strings.TrimSuffix(format, "\r\n"), a)
 	}
 }
 
