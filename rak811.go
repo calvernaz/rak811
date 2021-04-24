b73bf79ead018199003354c45c7e2a2dab449dc3
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2021-04-24 09:58:08 +0100

    add print

--- rak811.go
+++ rak811.go
@@ -516,6 +516,7 @@ func (l *Lora) SetUART(configuration string) (string, error) {
 }
 
 func readline(b []byte) (string, error) {
+	fmt.Printf("readline %v\n", string(b))
 	scanner := bufio.NewScanner(bytes.NewReader(b))
 	scanner.Split(bufio.ScanLines)
 
